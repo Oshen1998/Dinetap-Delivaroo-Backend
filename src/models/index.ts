50a5f6d2be4f3a949dcc8f8535a889de17df1a0d
Oshen Dikkumbura <oshen@echonlabs.com>
2025-09-19 19:22:30 +0530

    improvements

--- src/models/index.ts
+++ src/models/index.ts
@@ -38,7 +38,6 @@ const { User, Restaurant, Category, Dish, Order, OrderItem, RefreshToken } = mod
 Restaurant.hasMany(Category, { foreignKey: 'restaurantId', onDelete: 'CASCADE' });
 Category.belongsTo(Restaurant, { foreignKey: 'restaurantId' });
 
-// Add this missing association
 Restaurant.hasMany(Order, { foreignKey: 'restaurantId', onDelete: 'CASCADE' });
 Order.belongsTo(Restaurant, { foreignKey: 'restaurantId' });
 
