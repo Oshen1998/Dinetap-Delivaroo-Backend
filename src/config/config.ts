dce48f1216e60a7195ab4b9e643f3eed2b320792
Oshen Dikkumbura <oshen@echonlabs.com>
2025-09-25 03:57:25 +0530

    remove keys

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,9 +4,9 @@ dotenv.config();
 
 const config = {
   development: {
-    username: process.env['DB_USER'] || 'root',
-    password: process.env['DB_PASS'] || 'secret',
-    database: process.env['DB_NAME'] || 'delivaroo',
+    username: process.env['DB_USER'],
+    password: process.env['DB_PASS'],
+    database: process.env['DB_NAME'],
     host: process.env['DB_HOST'] || '127.0.0.1',
     port: Number(process.env['DB_PORT'] || 3306),
     dialect: 'mysql' as Dialect,
