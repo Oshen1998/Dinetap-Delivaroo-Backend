0409261f0c9d9b02ac1aae2c103f72ecbc6d093c
Oshen Dikkumbura <oshen@echonlabs.com>
2025-09-25 12:40:28 +0530

    change access token duration

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -4,6 +4,6 @@ dotenv.config();
 export const jwtConfig = {
   accessSecret: process.env['JWT_ACCESS_SECRET'],
   refreshSecret: process.env['JWT_REFRESH_SECRET'] || 'refresh-secret',
-  accessExpiresIn: process.env['JWT_ACCESS_EXP'] || '1d', // this should be 10-15/ dev purpose increase that amount
+  accessExpiresIn: process.env['JWT_ACCESS_EXP'] || '30m',
   refreshExpiresIn: process.env['JWT_ACCESS_EXP'] || '30d',
 };
